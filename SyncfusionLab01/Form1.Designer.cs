63e2a93f4e8cb755d9e4da600dd90ec2b1270cf3
LAPTOP-5QSR3R04\HP <ppman2702@gmail.com>
2024-06-04 14:46:37 +0700

    Add project files.

---
+++ SyncfusionLab01/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SyncfusionLab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfBarcode1 = new Syncfusion.Windows.Forms.Barcode.SfBarcode();
+            this.SuspendLayout();
+            // 
+            // sfBarcode1
+            // 
+            this.sfBarcode1.DarkBarColor = System.Drawing.Color.Black;
+            this.sfBarcode1.DisplayText = true;
+            this.sfBarcode1.LightBarColor = System.Drawing.Color.White;
+            this.sfBarcode1.Location = new System.Drawing.Point(298, 132);
+            this.sfBarcode1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sfBarcode1.Name = "sfBarcode1";
+            this.sfBarcode1.Size = new System.Drawing.Size(198, 187);
+            this.sfBarcode1.Symbology = Syncfusion.Windows.Forms.Barcode.BarcodeSymbolType.QRBarcode;
+            this.sfBarcode1.SymbologySettings = null;
+            this.sfBarcode1.TabIndex = 0;
+            this.sfBarcode1.TextAlignment = Syncfusion.Windows.Forms.Barcode.BarcodeTextAlignment.Center;
+            this.sfBarcode1.TextColor = System.Drawing.Color.Black;
+            this.sfBarcode1.TextGapHeight = 0F;
+            this.sfBarcode1.Load += new System.EventHandler(this.sfBarcode1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sfBarcode1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Barcode.SfBarcode sfBarcode1;
+    }
+}
+
---
+++ SyncfusionLab01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms.Barcode;
+
+namespace SyncfusionLab01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sfBarcode1_Load(object sender, EventArgs e)
+        {
+            SfBarcode sfDataGrid1 = new SfBarcode();
+            sfBarcode1.Text = "https://help.syncfusion.com/windowsforms/barcode";
+            this.Controls.Add(sfBarcode1);
+            
+            this.sfBarcode1.DarkBarColor = System.Drawing.Color.FromArgb(0, 0, 0);
+
+            Code39Setting code39Settings = new Code39Setting();
+            code39Settings.BarHeight = 100;
+            code39Settings.NarrowBarWidth = 1;
+            this.sfBarcode1.SymbologySettings = code39Settings;
+
+
+        }
+    }
+}
